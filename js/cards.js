f537cfb08e4d92c1d5d63a117378ea882fe0b541
zpp666123-del <zpp666123@gmail.com>
2026-07-21 11:31:59 +0800

    模块化改造

--- js/cards.js
+++ js/cards.js
@@ -1,9 +1,11 @@
-(function () {
-  anime.animate(".card", {
+import { animate, stagger } from "https://cdn.jsdelivr.net/npm/animejs@4/+esm";
+
+export function initCardsAnim() {
+  animate(".card", {
     opacity: [0, 1],
     translateY: [24, 0],
-    delay: anime.stagger(120),
+    delay: stagger(120),
     duration: 700,
     ease: "outBack",
   });
-})();
+}
